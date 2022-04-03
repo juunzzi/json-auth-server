982908b6cc41c46aca08b46604dd98fccf385649
junzzi <jjh01027717502@gmail.com>
2022-04-03 15:08:18 +0900

    배포

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-// /!\ Bind the router db to the app
 app.db = router.db;
 
-// You must apply the auth middleware before the router
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(auth);
 app.use(router);
-app.listen(process.env.PORT || 5000);
+
+app.listen(process.env.PORT || 3000);
